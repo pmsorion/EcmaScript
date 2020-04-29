0c1180086ae44ab24c70e20c288c722def36b891
Francisco Garcia <garciafran@hotmail.com>
2020-04-29 18:41:03 -0500

    Clases, modulos y generadores

--- src/es6/index.js
+++ src/es6/index.js
@@ -154,3 +154,56 @@ helloPromise()
     .then(response => console.log(response))
     .then( () => console.log('hola mundo'))
     .catch(error => console.log(error));
+
+
+//clases
+//programacion orientada a objetos
+
+class calculator {
+    constructor () {
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+    // estos son metodos
+    sum(valueA, valueB) {
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueA + this.valueB
+    }
+
+    multiplica(valueA, valueB) {
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueA * this.valueB
+    }
+}
+
+const calc = new calculator();
+
+console.log(calc.sum(2, 5));
+console.log(calc.multiplica(2, 5));
+
+
+//import y export
+import { hello } from './module';
+
+hello();
+
+//generators
+
+function* helloWord() {
+    if (true) {
+        yield 'Hello, ';
+    }
+
+    if (true) {
+        yield 'World';
+    }
+};
+
+const generatorHello = helloWord();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+
+//este caso es muy utili para fibonacci
