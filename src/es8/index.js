1202ceac13eb1ced7a6cbc7a854405ae91a3fdab
Francisco Garcia <garciafran@hotmail.com>
2020-04-29 22:32:36 -0500

    async - await

--- src/es8/index.js
+++ src/es8/index.js
@@ -32,3 +32,32 @@ console.log('food'.padEnd(12, '  ----'));
 const obj = {
     name: 'Fran',
 }
+
+const helloworld = () => {
+    return new Promise((resolve, reject) => {
+        (false) 
+            ? setTimeout(() => {
+            resolve('Hello World')
+        }, 3000)
+            : reject(new Error('Test error'))
+    })
+}
+
+const helloAsync = async () => {
+    const hello = await helloworld();
+    console.log(hello);
+}
+
+helloAsync();
+
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloworld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+anotherFunction();
